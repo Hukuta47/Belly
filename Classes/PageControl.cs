0c4bdc7beee4a8cc2437a65154bef1b3a20170cb
Никита Зелтынь <Hukuuuuuuuta47@gmail.com>
2024-08-03 09:38:10 +0900

    На этапе создания вклаки "Музыка"

---
+++ Classes/PageControl.cs
@@ -0,0 +1,28 @@
+﻿using Belly.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Belly.Classes
+{
+    public class PageControl
+    {
+        Page musicEditor = new MusicEditor();
+
+        static Frame mainFrame;
+
+        public enum Pages
+        {
+            musicEditor
+        }
+
+
+        static void ChangePage()
+        {
+
+        }
+    }
+}
