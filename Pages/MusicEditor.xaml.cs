7fae2779f6bb5e3a5902aba1b6558fef5539da25
Никита Зелтынь <Hukuuuuuuuta47@gmail.com>
2024-08-06 23:54:17 +0900

    Был сделан Drag 'n' Drop # Надо будет добавить строку для сортировки по алфавиту

--- Pages/MusicEditor.xaml.cs
+++ Pages/MusicEditor.xaml.cs
@@ -1,7 +1,11 @@
 ﻿
 using Belly.Classes.StaticClasses;
+using Belly.Dialogs;
 using Belly.Objects;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -12,24 +16,91 @@ namespace Belly.Pages
         public MusicEditor()
         {
             InitializeComponent();
-            DataGrid5Min.ItemsSource = musicLists.LoadList("5 min\\listInfo.json");
-            DataGrid10Min.ItemsSource = musicLists.LoadList("10 min\\listInfo.json");
-            DataGrid40Min.ItemsSource = musicLists.LoadList("40 min\\listInfo.json");
-
         }
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            musicLists.RefreshLists();
+
         }
         private void SaveClick(object sender, RoutedEventArgs e)
         {
-            musicLists.SaveLists
-            (
-                (List<Track>)DataGrid5Min.ItemsSource, 
-                (List<Track>)DataGrid10Min.ItemsSource, 
-                (List<Track>)DataGrid40Min.ItemsSource
-            );
+
         }
+        private void Page_Drop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            AddTrackWindow addTrackWindow = new AddTrackWindow();
+            
+            if (addTrackWindow.ShowDialog() == true)
+            {
+                foreach (string file in files)
+                {
+                    if (Path.GetExtension(file).Equals(".mp3", StringComparison.OrdinalIgnoreCase))
+                    {
+                        if (addTrackWindow.Accept5min)
+                        {
+                            File.Copy(file, $"5 min\\{Path.GetFileName(file)}", true);
+                            musicLists.Folder5Min.Add(new Track($"5min\\{file}"));
+                            musicLists.Folder5Min = RemoveDuplicates(musicLists.Folder5Min);
+
+
+                            var json = JsonConvert.DeserializeObject<List<Track>>(File.ReadAllText("5 min\\listInfo.json"));
+                            json = RemoveDuplicates(json);
+                            json.Add(new Track($"5 min\\{file}"));
+                            // TODO: sort
+                            var jsonWrit = JsonConvert.SerializeObject(json, Formatting.Indented);
+                            File.WriteAllText("5 min\\listInfo.json", jsonWrit);
+                        }
+                        if (addTrackWindow.Accept10min)
+                        {
+                            File.Copy(file, $"10 min\\{Path.GetFileName(file)}", true);
+                            musicLists.Folder10Min.Add(new Track($"5min\\{file}"));
+                            musicLists.Folder10Min = RemoveDuplicates(musicLists.Folder10Min);
+
+                            var json = JsonConvert.DeserializeObject<List<Track>>(File.ReadAllText("5 min\\listInfo.json"));
+                            json = RemoveDuplicates(json);
+                            json.Add(new Track($"10 min\\{file}"));
+                            // TODO: sort
+                            var jsonWrit = JsonConvert.SerializeObject(json, Formatting.Indented);
+                            File.WriteAllText("10 min\\listInfo.json", jsonWrit);
+                        }
+                        if (addTrackWindow.Accept40min)
+                        {
+                            File.Copy(file, $"40 min\\{Path.GetFileName(file)}", true);
+                            musicLists.Folder40Min.Add(new Track($"5min\\{file}"));
+                            musicLists.Folder40Min = RemoveDuplicates(musicLists.Folder40Min);
+
+                            var json = JsonConvert.DeserializeObject<List<Track>>(File.ReadAllText("5 min\\listInfo.json"));
+                            json = RemoveDuplicates(json);
+                            json.Add(new Track($"40 min\\{file}"));
+                            // TODO: sort
+                            var jsonWrit = JsonConvert.SerializeObject(json, Formatting.Indented);
+                            File.WriteAllText("40 min\\listInfo.json", jsonWrit);
+                        }
+                    }
+                }
+            }
+
+            DataGrid5Min.ItemsSource = musicLists.Folder5Min;
+            DataGrid10Min.ItemsSource = musicLists.Folder10Min;
+            DataGrid40Min.ItemsSource = musicLists.Folder40Min;
+
+        }
+
+        public static List<T> RemoveDuplicates<T>(List<T> list)
+        {
+            HashSet<T> seenElements = new HashSet<T>();
+            List<T> uniqueList = new List<T>();
+
+            foreach (T item in list)
+            {
+                if (seenElements.Add(item))
+                {
+                    uniqueList.Add(item);
+                }
+            }
+
+            return uniqueList;
+        }
+
     }
 }
