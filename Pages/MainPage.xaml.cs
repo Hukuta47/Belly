f51c1463e2b4e8c154e95c7964884d220d9fea87
Никита Зелтынь (Ноутбук/Рабочий) <Hukuta47@yandex.ru>
2024-08-20 13:34:19 +0900

    Сделал размертку на главной вкладке

--- Pages/MainPage.xaml.cs
+++ Pages/MainPage.xaml.cs
@@ -12,35 +12,7 @@ namespace Belly.Pages
         {
             InitializeComponent();
 
-            cmbx.ItemsSource = sheduleList.basicSchedule.bells;
 
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            string pathToTrack = "";
-
-            switch (((Bell)cmbx.SelectedItem).Media.typeData)
-            {
-                case Enums.TypeData.Track:
-                    Bell tmp = (Bell)cmbx.SelectedItem;
-
-                    MessageBox.Show(tmp.Media.Name);
-
-                    pathToTrack = ((Track)((Bell)cmbx.SelectedItem).Media).Path;
-                    break;
-                case Enums.TypeData.Folder:
-                    pathToTrack = ((Folder)((Bell)cmbx.SelectedItem).Media).GetPriorityPath(musicLists.Min5);
-                    break;
-            }
-
-
-            var audioFile = new AudioFileReader(pathToTrack);
-            var outputDevice = new WaveOutEvent();
-
-
-            outputDevice.Init(audioFile);
-            outputDevice.Play();
         }
     }
 }
