bebdc67668b7761cdb7ce1f9381e94089e62f0d7
Никита <hukuta47@yandex.ru>
2024-10-03 12:23:49 +0900

    sync

---
+++ Dialogs/AddMediaFileWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace Belly.Dialogs
+{
+    public partial class AddMediaFileWindow : Window
+    {
+        public bool SelectedMusic, SelectedAudio;
+        public AddMediaFileWindow()
+        {
+            InitializeComponent();
+        }
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void Radio_SelectedMusic_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedMusic = true;
+            SelectedAudio = false;
+        }
+        private void Radio_SelectedAudio_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedMusic = false;
+            SelectedAudio = true;
+        }
+        private void Accept_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
