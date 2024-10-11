bfaab630932d0c95f8f7fc2a56417bf48042b877
Никита <hukuta47@yandex.ru>
2024-10-12 06:15:41 +0900

    sync

--- Classes/StaticClasses/Player.cs
+++ Classes/StaticClasses/Player.cs
@@ -1,8 +1,13 @@
 ﻿
 using System.Threading.Tasks;
-using Belly.Enums;
 using Belly.Objects;
+using Belly;
 using NAudio.Wave;
+using System.Windows.Documents;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Data;
 
 namespace Belly.Classes.StaticClasses
 {
@@ -11,18 +16,62 @@ namespace Belly.Classes.StaticClasses
 
         public SettingsValues _settings;
 
-        public WaveOut waveOut;
+        public WaveOutEvent OutputDevice;
 
-        public Mp3FileReader reader;
+        public AudioFileReader AudioFile;
 
 
-        public async Task Play(MediaFile PathMediaFile, int miliseconds)
+        public Player() => OutputDevice = new WaveOutEvent();
+
+        public async Task PlayMusic(int miliseconds)
         {
+            int countMilliseconds = miliseconds;
+            while (true)
+            {
+                if (AudioFile != null)
+                {
+                    AudioFile.Dispose();
+                }
+                if (OutputDevice.PlaybackState != PlaybackState.Playing)
+                {
+                    AudioFile = new AudioFileReader(GetPathOnPriority(MainWindow.MusicList));
+
+                    OutputDevice.Init(AudioFile);
+                    OutputDevice.Play();
+                }
+                if (countMilliseconds < 0)
+                {
+                    AudioFile.Dispose ();
+                    OutputDevice.Dispose();
+                    break;
+                }
+                else
+                {
+                    await Task.Delay(1);
+                    countMilliseconds--;
+                }
+            }
 
         }
         public async Task Play(MediaFile PathMediaFile)
         {
 
+        }
+        string GetPathOnPriority(List<Music> items)
+        {
+            int totalWeight = items.Sum(item => item.Priority);
+            Random random = new Random();
+            int randomValue = random.Next(totalWeight);
+
+            foreach (Music music in items)
+            {
+                if (randomValue < music.Priority)
+                {
+                    return music.Path;
+                }
+                randomValue -= music.Priority;
+            }
+            return default(string);
         }
     }
 
