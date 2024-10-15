b9c5d7d4c137070a444fdb48e7abc230f82f51af
Никита <hukuta47@yandex.ru>
2024-10-15 10:10:04 +0900

    sync

---
+++ Objects/Day.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+
+namespace Belly.Objects
+{
+    public class Day : TabItem
+    {
+        public Day(DayOfWeek dayOfWeek)
+        {
+            switch (dayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                    Header = "Понидельник";
+                    break;
+                case DayOfWeek.Tuesday:
+                    Header = "Вторник";
+                    break;
+                case DayOfWeek.Wednesday:
+                    Header = "Среда";
+                    break;
+                case DayOfWeek.Thursday:
+                    Header = "Четверг";
+                    break;
+                case DayOfWeek.Friday:
+                    Header = "Пятница";
+                    break;
+                case DayOfWeek.Saturday:
+                    Header = "Суббота";
+                    break;
+            }
+        }
+        
+    }
+}
