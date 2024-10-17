a51a9b5e6cedb5de83ef336656ee8e0cc8ae25c2
Никита <hukuta47@yandex.ru>
2024-10-17 13:50:25 +0900

    sync

--- Pages/MainPage.xaml.cs
+++ Pages/MainPage.xaml.cs
@@ -15,6 +15,7 @@ namespace Belly.Pages
             InitializeInterface();
             timeText = TimeLabel;
             TimeLabel.Content = MainWindow.TimeNow.ToString();
+            Combobox_SelectSchedule.SelectionChanged += Schedule_SelectionChanged;
 
         }
         void InitializeInterface()
@@ -22,11 +23,35 @@ namespace Belly.Pages
             ListBox_WeekList.ItemsSource = MainWindow.Week;
             Combobox_SelectSchedule.ItemsSource = MainWindow.ScheduleList;
 
+
+            int _numDay = (int)DateTime.Now.DayOfWeek - 1;
+            if (_numDay <= 5)
+            {
+                ListBox_WeekList.SelectedIndex = _numDay;
+            }
+            else
+            {
+                ListBox_WeekList.SelectedIndex = -1;
+            }
         }
 
         private void DayOfWeekChanged(object sender, SelectionChangedEventArgs e)
         {
             Combobox_SelectSchedule.SelectedItem = ((Day)ListBox_WeekList.SelectedItem).Schedule;
+        }
+        private void Schedule_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ((Day)ListBox_WeekList.SelectedItem).Schedule = (Schedule)Combobox_SelectSchedule.SelectedItem;
+            DataGrid_IssueList.ItemsSource = ((Schedule)Combobox_SelectSchedule.SelectedItem).Issues;
+        }
+        public void ResetData()
+        {
+            //DataGrid_IssueList = null;
+
+        }
+        public void RecoveryData()
+        {
+            //if ((Schedule)Combobox_SelectSchedule.SelectedItem != null) DataGrid_IssueList.ItemsSource = ((Schedule)Combobox_SelectSchedule.SelectedItem).Issues;
         }
     }
 }
